7dc2737259cab6bb9b46aabc3f94097b24f099a0
George Snelling <georgesn@gmail.com>
2025-03-13 21:25:53 -0700

    not working -- commit to share

--- play.py
+++ play.py
@@ -1,21 +1,33 @@
 
-line_max_len = 5
-line_count = 0
+import sys
 
 # validate idividual lines
-def validate(line):
+def validate_line(line:str, line_count:int):
     line = line.strip()  #remove trailing newline
     if len(line) > line_max_len:
         raise ValueError ('line ' + str(line_count + 1) + ' beginning with "' +
                 line + '" is too long. Exiting.')
     return line
 
-f = open('words.txt', 'r')
-while line := f.readline(line_max_len + 2):
-    line = validate(line)
-    line_count += 1
-    print(line)
+def run(file_name:str = 'words.txt', line_max_len:int = 1000):
 
-print('Processed ' + str(line_count) + ' lines')
-f.close()
+    line = ''
+    line_count = 0
+
+    f = open(file_name, 'r')
+    
+    while line := f.readline(line_max_len + 2):  # one for the newline, one for over max_len
+        line = validate(line, line_count)
+        line_count += 1
+        print(line)
+
+    print('Processed ' + str(line_count) + ' lines')
+    f.close()
+
+run()
+    file_name = 'words.txt'
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+
+
 
