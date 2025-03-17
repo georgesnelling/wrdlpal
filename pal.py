bef410a7a4a4432fffec28208c324b81a0cc6f05
George Snelling <georgesn@gmail.com>
2025-03-16 23:32:56 -0700

    working with point ranking and pretty print

--- pal.py
+++ pal.py
@@ -1,21 +1,33 @@
 
 import sys
 
-
-def run(file_name:str = 'words.txt', line_max_len:int = 5):
-
-    if len(sys.argv) > 1:
-        file_name = sys.argv[1]
+def run():
 
     line = ''
     line_count = 0
-
-    letters = 'abcdefghijklmnopqrstuvwxyz'
+    letter_count = 0
+    line_max_len = 5
+    tot_letter_points = 1000
+    tot_pair_points = 10000
+    pair_points_cutoff = 10
+    file_name = 'words.txt'
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
 
     # Initialize pairs dict
     pairs = {}
+    letters = 'abcdefghijklmnopqrstuvwxyz'
     for letter in letters:
-        pairs[letter] = {'count':0, 'left': {}, 'right': {}}
+        pairs[letter] = {
+            'count':0, 
+            'points': 0, 
+            'left': {}, 
+            'left_sorted': [],
+            'left_points': [], 
+            'right': {},
+            'right_sorted': [],
+            'right_points': []
+        }
 
 
     # validate idividual lines
@@ -39,16 +51,49 @@ def run(file_name:str = 'words.txt', line_max_len:int = 5):
 
             if pairs[letter]['count'] == 0:
                 continue
+            
+            print('\n' + letter + ': ' + str(pairs[letter]['points']) + '/' + str(tot_letter_points))
 
-            print('\n' + letter + ': ' + str(pairs[letter]['count']))
+            print('Left: ', end='')
+            for point in pairs[letter]['left_points']:
+                print(' ' + point[0] + ':' + str(point[1]), end='')
+            print()
 
-            print('Left: ')
-            for left in pairs[letter]['left']:
-                print(left + ': ' + str(pairs[letter]['left'][left]))
+            print('Right:', end='')
+            for point in pairs[letter]['right_points']:
+                print(' ' + point[0] + ':' + str(point[1]), end='')
+            print()
 
-            print('Right: ')
-            for right in pairs[letter]['right']:
-                print(right + ': ' + str(pairs[letter]['right'][right]))
+    
+    def sort_pairs_by_count(pairs):
+        for letter in pairs:
+            pairs[letter]['left_sorted'] = list(sorted(pairs[letter]['left'].items(), key=lambda x:x[1], reverse=True))
+            pairs[letter]['right_sorted'] = list(sorted(pairs[letter]['right'].items(), key=lambda x:x[1], reverse=True))
+
+
+    # Calculate normalized points for letters and pairs independent of sample size
+    def score(pairs, letter_count):
+
+        for letter in pairs:
+            
+            # Calculate the points for the letter itself
+            pairs[letter]['points'] = round(pairs[letter]['count'] / letter_count * tot_letter_points)
+
+            # TODO:  create helper to get rid of duped logic
+            left_points = []
+            for toup in pairs[letter]['left_sorted']:
+                pair_points = round(toup[1] / letter_count * tot_pair_points)
+                if pair_points >= pair_points_cutoff:
+                    left_points.append((toup[0], pair_points))
+            pairs[letter]['left_points'] = left_points
+                
+            right_points = []
+            for toup in pairs[letter]['right_sorted']:
+                pair_points = round(toup[1] / letter_count * tot_pair_points)
+                if pair_points >= pair_points_cutoff:
+                    right_points.append((toup[0], pair_points))
+            pairs[letter]['right_points'] = right_points
+                
 
 
     # count the letter pairs in a line, including BOL and EOL as a letter
@@ -56,7 +101,7 @@ def run(file_name:str = 'words.txt', line_max_len:int = 5):
 
         def increment(c:str, left:str, right:str):
 
-            # increment the letter count
+            # increment this particular letter count
             pairs[c]['count'] += 1
 
             # create the dict keys if they don't exist and set them to 0
@@ -100,11 +145,16 @@ def run(file_name:str = 'words.txt', line_max_len:int = 5):
     while line := f.readline(line_max_len + 2):  # one for the newline, one for over max_len
         line = validate_line(line)
         line_count += 1
-        print(line)
+        letter_count += len(line)
         count_pairs(line)
+        print(line)
 
-    print('Processed ' + str(line_count) + ' lines')
+    # sort dicts into lists by count descending
+    sort_pairs_by_count(pairs)
 
+    score(pairs, letter_count)
+
+    print('Processed ' + str(line_count) + ' lines and ' + str(letter_count) + ' letters')
     pretty_print(pairs)
 
     f.close()
