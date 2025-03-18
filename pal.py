74184481f3e8fc188cb4b0a0fedf4a384ab01071
George Snelling <georgesn@gmail.com>
2025-03-17 21:21:10 -0700

    updated pal.txt with current output

--- pal.py
+++ pal.py
@@ -10,21 +10,24 @@ def run():
     tot_letter_points = 1000
     tot_pair_points = 10000
     pair_points_cutoff = 10
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    consonants = 'bcdfghjklmnpqrstvwxz'
     file_name = 'words.txt'
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
 
     # Initialize pairs dict
     pairs = {}
-    letters = 'abcdefghijklmnopqrstuvwxyz'
     for letter in letters:
         pairs[letter] = {
             'count':0, 
             'points': 0, 
             'left': {}, 
             'left_points': [], 
+            'cleft_points': [],
             'right': {},
-            'right_points': []
+            'right_points': [],
+            'cright_points': []
         }
 
 
@@ -71,12 +74,14 @@ def run():
             print('\n' + letter + ':', (pairs[letter]['points']))
 
             # Print the left and right pairs and their points
-            keys = [('Left: ', 'left_points'), ('Right:', 'right_points')]
+            keys = [('Left: ', 'left_points'), ('cLeft: ', 'cleft_points'), 
+                    ('Right:', 'right_points'), ('cRight:', 'cright_points')]
             for k in keys:
-                print(k[0], end='')
-                for point in pairs[letter][k[1]]:
-                    print(' ' + point[0] + ':' + str(point[1]), end='')
-                print()
+                if (len(pairs[letter][k[1]])):
+                    print(k[0], end='')
+                    for point in pairs[letter][k[1]]:
+                        print(' ' + point[0] + ':' + str(point[1]), end='')
+                    print()
 
     
     # Calculate normalized points for letters and pairs independent of sample size
@@ -87,7 +92,7 @@ def run():
             # Calculate the points for the letter itself
             pairs[letter]['points'] = round(pairs[letter]['count'] / letter_count * tot_letter_points)
 
-            keys = [('left', 'left_points'), ('right', 'right_points')]
+            keys = [('left', 'left_points', 'cleft_points'), ('right', 'right_points', 'cright_points')]
 
             for k in keys:
                 pair_points = 0
@@ -95,6 +100,9 @@ def run():
                     pair_points = round(pairs[letter][k[0]][pair_let] / letter_count * tot_pair_points)
                     if pair_points >= pair_points_cutoff:
                         pairs[letter][k[1]].append((pair_let, pair_points))
+                        if letter in consonants and pair_let in consonants:
+                            pairs[letter][k[2]].append((pair_let, pair_points))
+
 
                 # sort points array by pair count descending
                 pairs[letter][k[1]].sort(key=lambda x: x[1], reverse=True)
